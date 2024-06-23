db8d4c7652dc936f9baf59e6da8427c0f88d94af
mohamed-salah-dev <mohamedslh.3310@gmail.com>
2024-06-23 11:52:26 +0300

    change on models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,5 +7,81 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+
     use HasFactory;
-}
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(UserReceiver::class, 'receiverId');
+    }
+
+
+
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class, 'countryId');
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class, 'stateId');
+    }
+
+    public function deliveryArea()
+    {
+        return $this->belongsTo(DeliveryRegion::class, 'deliveryRegionId');
+    }
+
+
+
+
+    public function store()
+    {
+        return $this->belongsTo(PickupStore::class, 'storeId');
+    }
+
+
+
+
+    public function orderEmployee()
+    {
+        return $this->belongsTo(Employee::class, 'orderEmployeeId');
+    }
+
+    public function paymentEmployee()
+    {
+        return $this->belongsTo(Employee::class, 'paymentEmployeeId');
+    }
+
+    public function refundEmployee()
+    {
+        return $this->belongsTo(Employee::class, 'refundEmployeeId');
+    }
+
+
+
+
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class, 'paymentId');
+    }
+
+
+
+
+    public function products()
+    {
+        return $this->hasMany(OrderProduct::class, 'orderId', 'id');
+    }
+
+
+
+} // end modal
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,25 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
-
-    public function category()
-    {
-
-        return $this->belongsTo(Category::class, 'categoryId');
-
-    } // end function
-
-
-
-    public function subCategory()
-    {
-
-        return $this->belongsTo(SubCategory::class, 'subCategoryId');
-
-    } // end function
-
-
-
-
-
-} // end model
+}
